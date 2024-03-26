a5a234a8c07ee640633571643f77fb94493b983e
Zioun <jionkhan0@gmail.com>
2024-03-26 15:55:13 +0600

    error Page added

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 import Home from './Components/Home/Home';
 import Root from './Components/Root/Root';
 import Details from './Components/Details/Details';
 import Listed from './Components/Listed/Listed';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root></Root>,
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
-        path: "/",
-        element: <Home></Home>,
-        loader: () => fetch("../books.json")
+        path: '/',
+        element: <Home />,
+        loader: () => fetch('../books.json'),
       },
       {
-        path: "/details/:bookId",
-        element: <Details></Details>,
-        loader: () => fetch("../books.json")
+        path: '/details/:bookId',
+        element: <Details />,
+        loader: () => fetch('../books.json'),
       },
       {
-        path: "/listed",
-        element: <Listed></Listed>,
-        loader: () => fetch("../books.json")
+        path: '/listed',
+        element: <Listed />,
+        loader: () => fetch('../books.json'),
       },
-    ]
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
