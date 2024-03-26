2d63eb45c3f0a4da9e52fd5506ab9bb31f6ed0a0
Zioun <jionkhan0@gmail.com>
2024-03-27 01:56:47 +0600

    404 page added

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            Oops
-        </div>
-    );
+  return (
+    <div className="flex justify-center h-screen items-center text-center">
+      <div className="border p-20 rounded-2xl bg-slate-50 ">
+        <h1 className="text-[200px]">404</h1>
+        <p className="text-[50px]">Page Not Found</p>
+        <Link to="/">
+          <button className="btn btn-neutral mt-10">Back To Home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
