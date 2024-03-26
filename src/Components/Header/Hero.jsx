44f9849e1a8e421275e471b733407ee385e09a07
Zioun <jionkhan0@gmail.com>
2024-03-27 01:45:43 +0600

    view the list added

--- src/Components/Header/Hero.jsx
+++ src/Components/Header/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
         />
         <div>
           <h1 className="text-5xl font-bold pd leading-[70px] max-w-[526px]">Books to freshen up your bookshelf</h1>
-          <button className="btn bg-[#23BE0A] hover:bg-[#23BE0A] text-white ws mt-[40px]">View The List</button>
+          <Link to="/listed"><button className="btn bg-[#23BE0A] hover:bg-[#23BE0A] text-white ws mt-[40px]">View The List</button></Link>
         </div>
       </div>
     </div>
